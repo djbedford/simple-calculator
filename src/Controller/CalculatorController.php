67458bbbf71b084e30a498780dd79896919a3dd1
David Bedford <beds2004@hotmail.com>
2018-01-24 13:28:50 +0000

    Set up form

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -2,9 +2,17 @@
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CalculatorController extends Controller {
-    public function show() {
-        return $this->render('calculator/calculator.html.twig');
+    public function index(Request $request) {
+        $form = $this->createFormBuilder()
+            ->add('input', TextType::class)
+            ->getForm();
+
+        return $this->render('calculator/calculator.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
